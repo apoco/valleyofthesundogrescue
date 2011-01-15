b9d16027b2672cb62faec025724f929af47bf1f3
dtuso <dtuso@godaddy.com>
2011-01-15 12:11:06 -0700

    Added new NewsStoryController.cs and associated views.

---
+++ code/VOTSDR.Admin.Web/Controllers/NewsStoryController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VOTSDR.Admin.Web.Controllers
+{
+    public class NewsStoryController : Controller
+    {
+        //
+        // GET: /NewsStory/
+
+        public ActionResult Index()
+        {
+            var db = new VOTSDR.Data.DataEntities();
+
+            var newsStories = db.NewsStories;
+            
+            return View(newsStories);
+        }
+
+        //
+        // GET: /NewsStory/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /NewsStory/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /NewsStory/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /NewsStory/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /NewsStory/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /NewsStory/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /NewsStory/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
